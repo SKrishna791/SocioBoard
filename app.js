3cd6afca5dd81b4ba9ba1adb74039a61895abb44
Sabari Krishna <Sabarikrishnaork@gmail.com>
2023-10-04 00:25:17 -0400

    Login routes and Routes folder added

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3003;
+const middleware = require('./middleware')
 
 const server = app.listen(port,() => console.log("Server listening on port " + port));
 
 app.set("view engine" , "pug");
 app.set("views", "views");
 
-app.get("/",(req, res, next) => {
+const loginRoute =  require('./routes/loginRoutes');
+
+app.use("/login", loginRoute );
+
+app.get("/",middleware.requireLogin, (req, res, next) => {
 
     var payload = {
         pageTitle: "Home"
